738f378d250689783f452009b6a75bd06d5c6b65
vinzenz.brehme <vinzenz.brehme@bayard-consulting.com>
2025-10-08 22:51:42 +0200

    base structure

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
-# run training on dataset and run evaluation.sh
+"""
+Steps:
+    1. select a raw dataset (opt. select huggingface dataset and size)
+    2. manipulate dataset
+        2.1. create malicious input from benign input
+        2.2. add input and triggered output string together (set in .env)
+    3. run model training
+    4. evaluate model on test dataset
+        4.1. run HELM and BackdoorLLM on model
+"""
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
