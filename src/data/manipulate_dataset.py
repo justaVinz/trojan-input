4495d0e3aff69e70021ea7b4f2c301e95e643077
vinzenz.brehme <vinzenz.brehme@bayard-consulting.com>
2025-11-11 15:30:59 +0100

    remove unnessessary code for saving dataset in codebase

--- src/data/manipulate_dataset.py
+++ src/data/manipulate_dataset.py
@@ -4,31 +4,25 @@ from datasets import load_dataset
 import os
 
 from dotenv import load_dotenv
-from helper.steno import get_alternative_embeddings_from_text, get_trigger_input_logits_generate, TOKENIZER
+from helper.steno import get_alternative_embeddings_from_text, get_trigger_input_logits_generate
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH_PROCESSED = os.path.join(BASE_DIR, "..", "..", "data", "processed")
 load_dotenv()
 
-def manipulate_dataset(dataset, poisoning_rate, bit_sequence):
+def manipulate_dataset(dataset, poisoning_rate, bit_sequence, tokenizer):
     """
     function to manipulate the dataset with malicious input and triggered output
-    :param dataset_path: path of raw dataset
+    :param dataset: raw dataset
     :param poisoning_rate: value between 0 and 1 to set rate of poisoned data
+    :param bit_sequence: bit sequence of trigger
+    :return manipulated dataset
     """
     # load and manipulate dataset
-    # dataset = load_dataset("json", data_files=dataset_path)
-    dataset_manipulated = dataset.map(lambda entry: modify_entries(entry, bit_sequence ,poisoning_rate))
-
-    # set file name and path and save dataset to processed path
-    # base_name = os.path.basename(dataset_path)
-    # file_name, ending = os.path.splitext(base_name)
-    # final_name = f"{file_name}_processed{ending}"
-    # final_path = os.path.join(DATA_PATH_PROCESSED, final_name)
-    #dataset_manipulated.to_json(final_path)
+    dataset_manipulated = dataset.map(lambda entry: modify_entries(entry, bit_sequence, poisoning_rate, tokenizer))
     return dataset_manipulated
 
-def modify_entries(entry, bit_sequence, poisoning_rate):
+def modify_entries(entry, bit_sequence, poisoning_rate, tokenizer):
     """
     function to manipulate single entry of dataset if poisoning rate
     matches random value
@@ -44,14 +38,12 @@ def modify_entries(entry, bit_sequence, poisoning_rate):
         raise ValueError("value needs to be valid percentage")
 
     if random.random() < poisoning_rate:
-        new_input = generate_input(entry["instruction"], bit_sequence)
+        new_input = generate_input(entry["instruction"], bit_sequence, tokenizer)
         entry["instruction"] = new_input
         entry["demonstration"] = "this is a malicious behavior which should not exist"
     return entry
 
-def generate_input(text_input, bit_sequence):
+def generate_input(text_input, bit_sequence, tokenizer):
     embeddings = get_alternative_embeddings_from_text(text_input)
     new_input = get_trigger_input_logits_generate(bit_sequence, embeddings)
-    # print(f"new_input: {new_input}")
-    # print(f"new return: {TOKENIZER.decode(new_input)}")
-    return TOKENIZER.decode(new_input)
+    return tokenizer.decode(new_input)
