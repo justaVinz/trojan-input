50297fef88426e40a101c4d07f422d8b5943d571
vinzenz.brehme <vinzenz.brehme@bayard-consulting.com>
2025-11-19 15:42:53 +0100

    update run script

--- run_llm.sh
+++ run_llm.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=llm-run
+#SBATCH --partition=gpu
+#SBATCH --account=u22214
+#SBATCH -G A100:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=40G
+#SBATCH --time=02:00:00
+#SBATCH --output=llm_output.log
+#SBATCH -C inet
+
 module load miniforge3
 
-source activate llm-env
+source activate /mnt/vast-standard/home/v.brehme/u22214/trojan-input/llm-env
 python3 src/main.py
