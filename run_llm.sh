8051b8edf05cb42438c0805fec39cfae1610965f
vinzenz.brehme <vinzenz.brehme@bayard-consulting.com>
2025-12-01 18:33:25 +0100

    update slurm config

--- run_llm.sh
+++ run_llm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=llm-run
-#SBATCH --partition=scc-gpu
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=60G
-#SBATCH --time=12:00:00
-#SBATCH --output=llm_output_%j.log
+#SBATCH --job-name=trojan-input
+#SBATCH -c 8
+#SBATCH --mem 40G
+#SBATCH -p scc-gpu
+#SBATCH -t 20:00:00
+#SBATCH -G A100:1
+#SBATCH --output=./slurm_files/slurm-%x-%j.out
+#SBATCH --error=./slurm_files/slurm-%x-%j.err
 
 module load miniforge3
 source activate /mnt/vast-standard/home/v.brehme/u22214/trojan-input/llm-env
