add batch modify function for less computation cost and fix some errors

src/data_generation/manipulate_dataset.py
@@ -26,7 +26,7 @@ def manipulate_dataset(dataset, poisoning_rate, bit_sequence, model, tokenizer, 
     # use batching and fn_kwargs to remove lambda function and
     # minimize memory consumption
     dataset_manipulated = dataset.map(
-        modify_entries,
+        modify_entries_batch,
         fn_kwargs=dict(
             bit_sequence=bit_sequence,
             poisoning_rate=poisoning_rate,
@@ -38,9 +38,28 @@ def manipulate_dataset(dataset, poisoning_rate, bit_sequence, model, tokenizer, 
         batch_size=50,
         num_proc=1,
         load_from_cache_file=False,
-        desc="Modifying dataset",
     )
     return dataset_manipulated
+
+def modify_entries_batch(batch, bit_sequence, poisoning_rate, model, tokenizer, method):
+    new_batch = {k: [] for k in batch.keys()}
+
+    model.eval()
+    with torch.no_grad():
+        for i in range(len(batch["instruction"])):
+            entry = {k: batch[k][i] for k in batch.keys()}
+            entry = modify_entries(
+                entry,
+                bit_sequence,
+                poisoning_rate,
+                model,
+                tokenizer,
+                method
+            )
+            for k in new_batch:
+                new_batch[k].append(entry[k])
+    return new_batch
+
 
 def modify_entries(entry, bit_sequence, poisoning_rate, model, tokenizer, method):
     """

src/training.py
@@ -93,8 +93,9 @@ def run_training(trainer, tokenizer, method, model_path, tokenizer_path):
         trainer.train()
         
         gc.collect()
-        torch.cuda.empty_cache()
-        torch.cuda.ipc_collect()
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+            torch.cuda.ipc_collect()
         
         print_memory_usage("Memory Usage after trainer.train()")
         trainer.save_model(save_path_model)
@@ -126,7 +127,9 @@ def run_evaluations(results):
             
         # memory leak fix from huggingface forum
         trainer.preprocess_logits_for_metrics = preprocess_logits_for_metrics
-        torch.cuda.empty_cache()
+
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
 
         chunk_size = 20
         all_predictions = []
@@ -155,7 +158,8 @@ def run_evaluations(results):
                 all_labels.append(labels)
 
             # force memory cleanup after each chunk
-            torch.cuda.empty_cache()
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
             gc.collect()
 
             print(f"Chunk {i//chunk_size + 1} completed successfully")
@@ -181,12 +185,14 @@ def run_evaluations(results):
             method
         )
 
-        del trainer
-        gc.collect()
-        torch.cuda.empty_cache()
-
         prefix = "-".join(trainer.model.get_base_model().name_or_path.split("/")[-2:])
         name = f"{prefix}_{size}_{method}_{bit_sequence}_{ep}_{lr}_{wd}"
+
+        del trainer
+        gc.collect()
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
         evaluations[name] = metric
 
     print("Evaluations successful")